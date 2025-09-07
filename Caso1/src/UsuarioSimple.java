839751d50f42bdfe7f6a7f8bdd038ee43bdecb6b
WEPORXS <villanuevadavid761@gmail.com>
2025-09-07 08:47:54 -0500

    Se realiza casi 1 - UsuarioSimple

--- Caso1/src/UsuarioSimple.java
+++ Caso1/src/UsuarioSimple.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class UsuarioSimple {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        System.out.print("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
 
-	}
+        System.out.print("Ingrese su edad: ");
+        int edad = scanner.nextInt();
 
+        System.out.println("\nHola, " + nombre + ". Tienes " + edad + " años.");
+
+        scanner.close();
+    }
 }
